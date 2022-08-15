1ad164fefacb7f3c6c1e6fe57038eb53219fa92f
daydy225 <dabelydadi@gmail.com>
2022-08-15 17:46:03 +0000

    phonebook backend step8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -27,8 +27,12 @@ let persons = [
   },
 ]
 
+morgan.token('body', req => JSON.stringify(req.body))
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 app.use(express.json())
-app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   res.send('hello world')
